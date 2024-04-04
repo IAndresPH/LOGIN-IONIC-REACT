cd631038bac2e876056662834de4816f4051f7a5
Andrés Molina <128313106+IAndresPH@users.noreply.github.com>
2024-04-04 09:32:52 -0500

    Se inicia y crea proyecto.

---
+++ Login-React/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Login-React',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
